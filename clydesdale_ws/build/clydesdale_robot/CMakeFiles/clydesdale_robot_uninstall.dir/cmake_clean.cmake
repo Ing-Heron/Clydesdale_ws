ed9cf2b5c342512eda6564d6e3581d925b46435b
Heron <heronsanchez26@gmail.com>
2024-02-09 14:12:32 -0600

    fixed bugs

--- clydesdale_ws/build/clydesdale_robot/CMakeFiles/clydesdale_robot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clydesdale_robot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clydesdale_robot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- clydesdale_ws/build/clydesdale_robot/CMakeFiles/clydesdale_robot_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for clydesdale_robot_uninstall.
