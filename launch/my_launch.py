2620d1cc82477ebb4746ebb66e7a4260233774dd
Heron <heronsanchez26@gmail.com>
2024-02-20 22:44:19 -0600

    fixed error of tf

--- launch/my_launch.py
+++ launch/my_launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
@@ -14,17 +14,19 @@ def generate_launch_description():
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('clydesdale_robot'))
     xacro_file = os.path.join(pkg_path,'description','clydesdale_robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    #robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
-    rviz_file = os.path.join(pkg_path, 'config', 'config.rviz')
-    rviz_config_file = DeclareLaunchArgument('rviz_config', default_value=rviz_file)
+    # rviz_file = os.path.join(pkg_path, 'config', 'config.rviz')
+    # rviz_config_file = DeclareLaunchArgument('rviz_config', default_value=rviz_file)
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -32,13 +34,13 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rviz_config')]
-    )
+    # rviz_node = Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     name='rviz2',
+    #     output='screen',
+    #     arguments=['-d', LaunchConfiguration('rviz_config')]
+    # )
 
     joint_state_publisher_gui_node = Node(
         package='joint_state_publisher_gui',
@@ -54,10 +56,14 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='Use ros2_control if true'),
         #rviz_config_file,
 
-        node_robot_state_publisher,
+        node_robot_state_publisher
         #rviz_node,
-        joint_state_publisher_gui_node,
+        #joint_state_publisher_gui_node,
     ])
 
